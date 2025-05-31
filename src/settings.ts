87ee4d576af3b7a1c17fe642296718a97758c4f4
Johan Sanneblad <johan@sanneblad.se>
2025-05-31 15:25:56 +0200

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -54,7 +54,7 @@ export const DEFAULT_SETTINGS: NotebookNavigatorSettings = {
     showFeatureImage: false,
     featureImageProperty: 'feature',
     // Folder display
-    showRootFolder: true,
+    showRootFolder: false,
     showFolderFileCount: true,
     // Appearance
     selectionColor: '#B3D9FF',
